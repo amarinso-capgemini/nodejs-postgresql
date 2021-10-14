23728e7bf25f2acf430aae1da6bf8ee8c4d611d6
Angel Marin <angel-luis.marin-soler@capgemini.com>
2021-10-14 11:16:38 +0000

    change pool to client

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
-})
+const { Client } = require('pg')
+process.env.PGUSER='postgres';
+process.env.PGHOST='localhost';
+process.env.PGPORT=5432;
+process.env.PGPASSWORD='postgres';
+process.env.PGDATABASE='postgres';
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/db', (req, res) => {
-  pool.query('SELECT * from demo', (err2, res2) => {
-  res.send('<html><pre>'+JSON.stringify(res2.rows[0]))
-  pool.end() 
-})
+ const client = new Client()
+  client.connect( (err2, res2)=>{
+	  client.query('SELECT * from demo', (err3, res3) => {
+	  res.send('<html>'+new Date()+'<pre>'+JSON.stringify(res3.rows[0]))
+  });
+  })
 })
 
 app.listen(port, () => {
