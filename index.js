901deb19d99f79e2e8f201c2ae8b312da53559f1
Angel Luis Marin Soler <angel-luis.marin-soler@capgemini.com>
2022-03-08 08:54:07 +0100

    add env variables

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const { Client } = require('pg')
-process.env.PGUSER='postgres';
-process.env.PGHOST='localhost';
-process.env.PGPORT=5432;
-process.env.PGPASSWORD='postgres';
-process.env.PGDATABASE='postgres';
+process.env.PGUSER=process.env.PGUSER||'postgres';
+process.env.PGHOST=process.env.PGHOST||'localhost';
+process.env.PGPORT=process.env.PGPORT||5432;
+process.env.PGPASSWORD=process.env.PGPASSWORD||'postgres';
+process.env.PGDATABASE=process.env.PGDATABASE||'postgres';
+const PORT=process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,7 +21,7 @@ app.get('/db', (req, res) => {
   })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 });
 
